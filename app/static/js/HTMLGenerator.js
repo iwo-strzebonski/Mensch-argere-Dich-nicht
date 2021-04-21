f2041d05bfc7113fbf92e2d77a5897908f0479f9
Iwo Strzeboński <iwo.strzebonski@gmail.com>
2021-04-21 13:43:37 +0200

    Czyszczenie listy graczy

--- app/static/js/HTMLGenerator.js
+++ app/static/js/HTMLGenerator.js
@@ -7,6 +7,8 @@ export class HTMLGenerator {
      * @param {Array} players - List of players in specific room
      */
     static addPlayerNames(players) {
+        for (let i = 0; i < 4; i++) document.getElementById('player' + i).innerText = ''
+
         players.forEach(player => {
             if (player === null) return
             document.getElementById('player' + player.color).innerText = player.name
